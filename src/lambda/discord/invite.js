0f353a41d62a4a6961f5f4fff1d9a7d6e9027aa3
Alex Crocker <hello@crock.dev>
2020-02-14 14:33:15 -0500

    Added src/lambda to source control

---
+++ src/lambda/discord/invite.js
@@ -0,0 +1,25 @@
+import { Client } from 'discord.js'
+
+exports.handler = async function(event, context, callback) {
+    
+    const bot = new Client()
+    await bot.login(process.env.BOT_TOKEN)
+
+    const guild = bot.guilds.get(process.env.GUILD_ID);
+    const channel = guild.channels.get(process.env.INVITE_CHANNEL_ID);
+
+    const invite = await channel.createInvite({
+        temporary: false,
+        maxUses: 1,
+        unique: true,
+    }, "Generated an invite for a new user!");
+
+    callback(null, {
+        statusCode: 301,
+        headers: {
+            Location: invite.url,
+            'Cache-Control': 'no-cache',
+        },
+        body: JSON.stringify({inviteCode: invite.code})
+	})
+}
